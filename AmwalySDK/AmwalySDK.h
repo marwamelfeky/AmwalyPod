289a4cea3650b477e3b07d577a22b03d480f94f2
marwamelfeky <marwamelfeky@gmail.com>
2023-08-08 00:48:45 +0300

    Initial Project setup

---
+++ AmwalySDK/AmwalySDK.h
@@ -0,0 +1,18 @@
+//
+//  AmwalySDK.h
+//  AmwalySDK
+//
+//  Created by Marwa Elfeky on 08/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AmwalySDK.
+FOUNDATION_EXPORT double AmwalySDKVersionNumber;
+
+//! Project version string for AmwalySDK.
+FOUNDATION_EXPORT const unsigned char AmwalySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmwalySDK/PublicHeader.h>
+
+
